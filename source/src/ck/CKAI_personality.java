3471ee15d081bac8512ad4b2032fe0b5974bfc5b
Logan M. Butler <loganck@outlyehosting.com>
2016-06-07 08:17:46 -0400

    Added all characteristics

--- source/src/ck/CKAI_personality.java
+++ source/src/ck/CKAI_personality.java
@@ -62,7 +62,96 @@ public class CKAI_personality {
 		class Unconcious_aspects {
 			
 		}
+		/* Personal Traits (Private & Public) */
+		class Family {
+			
+		}
+		class Posessions {
+			
+		}
+		class Profession {
+			
+		}
+		class Religion {
+			
+		}
+		class Marital_Stauts {
+			
+		}
+		class Lifestyle {
+			
+		}
+		class Values {
+			
+		}
+		class Values {
+			
+		}
+		class Beliefs {
+			
+		}
+		class Hobbies {
+			
+		}
+		class Education {
+			
+		}
+		class Childhood {
+			
+		}
+		class Birthplace {
+			
+		}
+		class Colleagues {
+			
+		}
+		class Friends {
+			
+		}
+		/* Personality Traits */
+		class Wants {
+			
+		}
+		class Hopes {
+			
+		}
+		class Fears {
+			
+		}
+		class Thoughts {
+			
+		}
+		class Memories {
+			
+		}
+		class Resentments {
 		
+		}
+		class Dreams {
+			
+		}
+		class Denial {
+			
+		}
+		/* Practice Traits */
+		class Says {
+			
+		}
+		class Plays_at {
+			
+		}
+		class Works_at {
+			
+		}
+		class Buys {
+			
+		}
+		class Wears {
+			
+		}
+		class Eats {
+			
+		}
 	}
 
 }
