94f002cd2c395529f1d74001c8e223a8d9934972
Logan M. Butler <loganck@outlyehosting.com>
2016-06-07 08:01:35 -0400

    Added main file "CKAI.java"

---
+++ source/src/ck/CKAI.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package ck;
+
+/**
+ * @author	loganCk
+ * @desc	cKAI is an Intelligent AI built with Java. This bot will know human interaction, and communication. Using a	
+ * 	    	3D world (eventually), this bot will interact with other similar bots. 
+ * 
+ *          TODO: add more
+ *
+ */
+public class CKAI {
+
+	/**
+	 * cKAI posses the following:
+	 * 	
+	 * 		TODO: add stuff as needed
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, my name is cKAI");
+
+	}
+
+}
